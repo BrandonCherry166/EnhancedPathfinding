0c549ae901bedf72657abad69578e84434f6395d
Steven Bruns <steven.bruns@mymail.champlain.edu>
2025-10-20 23:06:53 -0400

    Made some decent progress with the different placements.

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Drawing; //Might cause conflict
+//using System.Drawing; //Might cause conflict
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class GridManager : MonoBehaviour
 {
     // Enum for placing elements
     public enum PlacementMode
-    { 
+    {
         None,
         PlaceObstacle,
         SetSource,
@@ -51,7 +52,10 @@ public class GridManager : MonoBehaviour
 
             if (Input.GetMouseButtonDown(0))
             {
-                PlaceObject();
+                if (!EventSystem.current.IsPointerOverGameObject())
+                {
+                    PlaceObject();
+                }
             }
 
             /*
@@ -63,163 +67,223 @@ public class GridManager : MonoBehaviour
             }
             */
         }
-        void CreateGhostObject(GameObject obstaclePrefab)
+    }
+    void CreateGhostObject(GameObject obstaclePrefab)
+    {
+        // Destroy any old ghosts
+        if (ghostObject != null)
         {
-            // Destroy any old ghosts
-            if (ghostObject != null)
-            {
-                Destroy(ghostObject);
-            }
-
-            ghostObject = Instantiate(obstaclePrefab);
-            ghostObject.GetComponent<Collider>().enabled = false;
-            ghostObject.name = "Ghost";
-
-            Renderer[] renderers = ghostObject.GetComponentsInChildren<Renderer>();
-
-            foreach (Renderer renderer in renderers)
-            {
-                Material mat = renderer.material;
-                UnityEngine.Color color = mat.color;
-                color.a = 0.5f;
-                mat.color = color;
-
-                mat.SetFloat("_Mode", 2);
-                mat.SetInt("_ScrBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
-                mat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
-                mat.SetInt("_ZWrite", 0);
-                mat.DisableKeyword("_ALPHATEST_ON");
-                mat.EnableKeyword("_ALPHABLEND_ON");
-                mat.DisableKeyword("_ALPHAPREMULTIPLY_ON");
-                mat.renderQueue = 3000;
-            }
+            Destroy(ghostObject);
         }
 
-        void UpdateGhostPosition()
+        ghostObject = Instantiate(obstaclePrefab);
+        ghostObject.GetComponent<Collider>().enabled = false;
+        ghostObject.name = "Ghost";
+
+        Renderer[] renderers = ghostObject.GetComponentsInChildren<Renderer>();
+
+        foreach (Renderer renderer in renderers)
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Material mat = renderer.material;
+            UnityEngine.Color color = mat.color;
+            color.a = 0.5f;
+            mat.color = color;
 
-            if (Physics.Raycast(ray, out RaycastHit hit))
+            mat.SetFloat("_Mode", 2);
+            mat.SetInt("_ScrBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+            mat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+            mat.SetInt("_ZWrite", 0);
+            mat.DisableKeyword("_ALPHATEST_ON");
+            mat.EnableKeyword("_ALPHABLEND_ON");
+            mat.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+            mat.renderQueue = 3000;
+        }
+    }
+
+    void UpdateGhostPosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            Vector2Int gridCell = WorldToGrid(hit.point);
+            Vector3 snappedPos = GridToWorld(gridCell);
+            ghostObject.transform.position = snappedPos;
+
+            if (occupiedPositions.Contains(gridCell))
             {
-                Vector2Int gridCell = WorldToGrid(hit.point);
-                Vector3 snappedPos = GridToWorld(gridCell);
-                ghostObject.transform.position = snappedPos;
-
-                if (occupiedPositions.Contains(gridCell))
+                if (currentMode == PlacementMode.PlaceObstacle)
                 {
-                    if (currentMode == PlacementMode.PlaceObstacle)
-                    {
-                        SetGhostColor(UnityEngine.Color.red);
-                    }
+                    SetGhostColor(UnityEngine.Color.red);
                 }
-                else // Open Space
-                {
-                    // Change the color based on mode
-                    switch (currentMode)
-                    {
-                        case PlacementMode.PlaceObstacle:
-                            SetGhostColor(new UnityEngine.Color(1f, 1f, 1f, 0.5f));
-                            break;
-                        case PlacementMode.SetSource:
-                            SetGhostColor(new UnityEngine.Color(0f, 1f, 0f, 0.5f));
-                            break;
-                        case PlacementMode.SetTarget:
-                            SetGhostColor(new UnityEngine.Color(0f, 0f, 0f, 0.5f));
-                            break;
-                        case PlacementMode.PlaceAgent:
-                            SetGhostColor(new UnityEngine.Color(1f, 1f, 0f, 0.5f));
-                            break;
-                    }
-                    //SetGhostColor(new UnityEngine.Color(1f, 1f, 1f, 0.5f));
-                }
-
-
             }
-        }
-
-        void SetGhostColor(UnityEngine.Color color)
-        {
-            Renderer[] renderers = ghostObject.GetComponentsInChildren<Renderer>();
-
-            foreach (Renderer renderer in renderers)
+            else // Open Space
             {
-                Material mat = renderer.material;
-                mat.color = color;
+                // Change the color based on mode
+                switch (currentMode)
+                {
+                    case PlacementMode.PlaceObstacle:
+                        SetGhostColor(new UnityEngine.Color(1f, 1f, 1f, 0.5f));
+                        break;
+                    case PlacementMode.SetSource:
+                        SetGhostColor(new UnityEngine.Color(0f, 1f, 0f, 0.5f));
+                        break;
+                    case PlacementMode.SetTarget:
+                        SetGhostColor(new UnityEngine.Color(0f, 0f, 0f, 0.5f));
+                        break;
+                    case PlacementMode.PlaceAgent:
+                        SetGhostColor(new UnityEngine.Color(1f, 1f, 0f, 0.5f));
+                        break;
+                }
+                //SetGhostColor(new UnityEngine.Color(1f, 1f, 1f, 0.5f));
             }
-        }
 
-        /*
-        void PlaceObject()
+
+        }
+    }
+
+    void SetGhostColor(UnityEngine.Color color)
+    {
+        Renderer[] renderers = ghostObject.GetComponentsInChildren<Renderer>();
+
+        foreach (Renderer renderer in renderers)
         {
-            Vector2Int gridCell = WorldToGrid(ghostObject.transform.position);
+            Material mat = renderer.material;
+            mat.color = color;
+        }
+    }
+
+    /*
+    void PlaceObject()
+    {
+        Vector2Int gridCell = WorldToGrid(ghostObject.transform.position);
+        if (!occupiedPositions.Contains(gridCell))
+        {
+            Vector3 placePos = GridToWorld(gridCell);
+            Instantiate(obstaclePrefab, placePos, Quaternion.identity);
+            occupiedPositions.Add(gridCell);
+        }
+    }
+    */
+
+    // Gets the right placement to place
+    void PlaceObject()
+    {
+        Vector2Int gridCell = WorldToGrid(ghostObject.transform.position);
+
+        // Place Obstacles
+        if (currentMode == PlacementMode.PlaceObstacle)
+        {
             if (!occupiedPositions.Contains(gridCell))
             {
                 Vector3 placePos = GridToWorld(gridCell);
-                Instantiate(obstaclePrefab, placePos, Quaternion.identity);
-                occupiedPositions.Add(gridCell);
+                GameObject newObstacle = Instantiate(obstaclePrefab, placePos, Quaternion.identity);
+                spawnedObstacles.Add(newObstacle);
+                occupiedPositions.Add(gridCell); // The cell is filled
             }
         }
-        */
 
-        // Gets the right placement to place
-        void PlaceObject()
+        // Set Source
+        else if (currentMode == PlacementMode.SetSource)
         {
-            Vector2Int gridCell = WorldToGrid(ghostObject.transform.position);
-
-            // Place Obstacles
-            if (currentMode == PlacementMode.PlaceObstacle)
-            {
-                if (!occupiedPositions.Contains(gridCell))
-                {
-                    Vector3 placePos = GridToWorld(gridCell);
-                    GameObject newObstacle = Instantiate(obstaclePrefab, placePos, Quaternion.identity);
-                    spawnedObstacles.Add(newObstacle);
-                    occupiedPositions.Add(gridCell); // The cell is filled
-                }
-            }
-
-            // Set Source
-            else if (currentMode == PlacementMode.SetSource)
-            {
-                SetMarker(ref sourceInstance, sourcePrefab, ref sourcePos, gridCell);
-            }
-
-            // Set Target
-            else if (currentMode == PlacementMode.SetTarget)
-            {
-                SetMarker(ref targetInstance, targetPrefab, ref targetPos, gridCell);
-            }
-
-            // Place Agent
-            else if (currentMode == PlacementMode.PlaceAgent)
-            {
-                SetMarker(ref agentInstance, agentPrefab, ref sourcePos, gridCell);
-            }
+            SetMarker(ref sourceInstance, sourcePrefab, ref sourcePos, gridCell);
         }
 
-        // Using to help with placing.
-        void SetMarker(ref GameObject instance, GameObject prefab, ref Vector2Int? gridPos, Vector2Int newCell)
+        // Set Target
+        else if (currentMode == PlacementMode.SetTarget)
         {
-            // Clear old Pos
-            if (instance != null)
-            {
-                occupiedPositions.Remove(gridPos.Value);
-                Destroy(instance);
-            }
+            SetMarker(ref targetInstance, targetPrefab, ref targetPos, gridCell);
         }
 
-        Vector2Int WorldToGrid(Vector3 worldPos)
+        // Place Agent
+        else if (currentMode == PlacementMode.PlaceAgent)
         {
-            int x = Mathf.RoundToInt(worldPos.x / gridSize);
-            int z = Mathf.RoundToInt(worldPos.z / gridSize);
-
-            return new Vector2Int(x, z);
-
+            SetMarker(ref agentInstance, agentPrefab, ref sourcePos, gridCell);
         }
+    }
 
-        Vector3 GridToWorld(Vector2Int gridPos)
+    // Using to help with placing.
+    void SetMarker(ref GameObject instance, GameObject prefab, ref Vector2Int? gridPos, Vector2Int newCell)
+    {
+        // Clear old Pos
+        if (instance != null)
         {
-            return new Vector3(gridPos.x * gridSize, 0f, gridPos.y * gridSize);
+            occupiedPositions.Remove(gridPos.Value);
+            Destroy(instance);
         }
+
+        // Place and label taken
+        Vector3 placePos = GridToWorld(newCell);
+        instance = Instantiate(prefab, placePos, Quaternion.identity);
+        gridPos = newCell;
+        occupiedPositions.Add(newCell);
+    }
+
+    Vector2Int WorldToGrid(Vector3 worldPos)
+    {
+        int x = Mathf.RoundToInt(worldPos.x / gridSize);
+        int z = Mathf.RoundToInt(worldPos.z / gridSize);
+
+        return new Vector2Int(x, z);
+
+    }
+
+    Vector3 GridToWorld(Vector2Int gridPos)
+    {
+        return new Vector3(gridPos.x * gridSize, 0f, gridPos.y * gridSize);
+    }
+
+    public void ResetBoard()
+    {
+        // Destroy Spawned Obst
+        foreach (GameObject obstacle in spawnedObstacles)
+        {
+            Destroy(obstacle);
+        }
+        spawnedObstacles.Clear();
+
+        // Destory Markers
+        if (sourceInstance != null) Destroy(sourceInstance);
+        if (targetInstance != null) Destroy(targetInstance);
+        if (agentInstance != null) Destroy(agentInstance);
+
+        // Clear
+        occupiedPositions.Clear();
+        sourcePos = null;
+        targetPos = null;
+    }
+
+    public void PlaceObstacle()
+    {
+        currentMode = PlacementMode.PlaceObstacle;
+        CreateGhostObject(obstaclePrefab);
+    }
+
+    public void SetSource()
+    {
+        currentMode = PlacementMode.SetSource;
+        CreateGhostObject(sourcePrefab);
+    }
+
+    public void SetTarget()
+    {
+        currentMode = PlacementMode.SetTarget;
+        CreateGhostObject(targetPrefab);
+    }
+
+    public void PlaceAgent()
+    {
+        currentMode = PlacementMode.PlaceAgent;
+        CreateGhostObject(agentPrefab);
+    }
+
+    public void RunPathfinding()
+    {
+        if (!sourcePos.HasValue || !targetPos.HasValue)
+        {
+            return;
+        }
+
+        // Pathfinding logic here
     }
 }
